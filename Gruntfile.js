a3d1d4985f0278fff7fa59f286b6eee3a610b401
Caleb Thorsteinson <caleb.thorsteinson@gmail.com>
2014-06-27 13:49:58 -0700

    Added a Gruntfile for docs, linting, require
    The added gruntfile is built from previous configurations for yuidoc and
    require, and additionally adds JSHint, for linting. By using Grunt,
    these tasks are all automated, and make it easier to perform, as well as
    for new developers to get started.
    
    YUIDoc currently only targets the core documentation, rather than both.
    And require on focuses on the dev build instead of also including
    production. This is because I've not yet tested using Grunt for multiple
    targets.

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'scripts/ItemMirror.js'],
+      options: {
+        // options listed http://www.jshint.com/docs/options
+        globals: {
+          bitwise: true,
+          camelCase: true,
+          curly: true,
+          indent: 2,
+          newcap: true,
+          strict: true
+        }
+      }
+    },
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: "scripts",
+          name: "ItemMirror",
+          out: "../build/ItemMirror.dev.js",
+          optimize: "none",
+          useStrict: true,
+          enforceDefine: true,
+          wrap: {
+            startFile: ["LICENSE"]
+          },
+          //Introduced in 2.1.3: Some situations do not throw and stop the optimizer
+          //when an error occurs. However, you may want to have the optimizer stop
+          //on certain kinds of errors and you can configure those situations via
+          //this option
+          throwWhen: {
+            //If there is an error calling the minifier for some JavaScript,
+            //instead of just skipping that file throw an error.
+            optimize: true
+          }
+        }
+      }
+    },
+    yuidoc: {
+      compile: {
+        "name": "ItemMirror",
+        "description": "ItemMirror: a library for working with XooML to promote open cross platform tool usage",
+        "version": "0.8.2",
+        "url": "https://github.com/KeepingFoundThingsFound/itemMirrorWebClient",
+        "options": {
+          "linkNatives": "true",
+          "attributesEmit": "true",
+          "ignorePaths": ["dependencies", "./.hgcheck/hg-checklink-qJbfof", ".idea", "./ignore", "./test", "./documentation", "./build"],
+          "exclude": "build",
+          "paths": "./scripts",
+          "outdir": "./documentation-core"
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
+
+  grunt.registerTask('test', ['jshint', 'yuidoc']);
+  
+  grunt.registerTask('default', ['jshint', 'yuidoc', 'requirejs']);
+
+};
