750189467dc1d573a3b52e35b6d88a0b826a83dd
Caleb Thorsteinson <caleb.thorsteinson@gmail.com>
2014-06-27 16:09:23 -0700

    Deleted old configuration files
    The old process involved downloading the tools for each process manually
    and then executing them with shell scripts in bin. Each configuration
    file was totally unrelated to other files, and much of it was redundant.
    
    Now the configuration for all steps of the build process has been moved
    into Gruntfile.js. These outdated files have no purpose anymore.

--- bin/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-r.js -o ./config/dev.build.js;
-r.js -o ./config/pro.build.js;
-echo "XooML.js Require.JS Optimized";
--- bin/documentation.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-yuidoc . -c ./config/yuidoc.json;
-echo "Documentation Generated";
