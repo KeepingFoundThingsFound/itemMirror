d78961b46a97df11e0d52358b529c9821067fd71
Caleb Thorsteinson <caleb.thorsteinson@gmail.com>
2014-08-07 23:22:45 -0700

    Makes new XooMLEditor
    Previously the SyncDriver was incorrectly using the associations of the
    __itemMirror__ object for comparison with the itemDriver. This goes
    against the intended behavior because Sync is supposed to read the XooML
    file itself directly with the XooML driver, and then persist any
    changes.
    
    The modifications now will callback with the proper error if it's not
    synchronized, or simply callback false. It behaves as it should
    constructing a brand new fragmentEditor. Thanks to the flexibility of
    the new and improved editor, we're able to support this much more easily
    than before.

--- scripts/SyncDriver.js
+++ scripts/SyncDriver.js
@@ -15,13 +15,19 @@
  * @protected
  */
 define([
+  "./XooMLDriver",
   "./XooMLExceptions",
   "./XooMLConfig",
   "./XooMLUtil",
+  "./FragmentEditor",
+  "./AssociationEditor"
 ], function(
+  XooMLDriver,
   XooMLExceptions,
   XooMLConfig,
-  XooMLUtil) {
+  XooMLUtil,
+  FragmentEditor,
+  AssociationEditor) {
   "use strict";
 
   var self;
@@ -29,7 +35,6 @@ define([
   function SyncDriver(itemMirror) {
     var self = this;
     self._itemMirror = itemMirror;
-    self._fragmentEditor = itemMirror._fragmentEditor;
     self._itemDriver = itemMirror._itemDriver;
     self._xooMLDriver = itemMirror._xooMLDriver;
 
@@ -41,13 +46,9 @@ define([
     * @protected
     */
     function _nameCompare(a, b) {
-      if (a.name > b.name) {
-        return 1;
-      }
-      if (a.name < b.name) {
-        return -1;
-      }
-      return 0;
+      if (a.name > b.name) return 1;
+      else if (a.name < b.name) return -1;
+      else return 0;
     }
   }
 
@@ -68,11 +69,11 @@ define([
   self.sync = function(callback) {
     var self = this,
         items,
-        realAssociations,
-        memoryAssociations,
-        realNames,
-        memoryNames,
-        memoryIdx,
+        itemAssociations,
+        xooMLAssociations,
+        itemNames,
+        xooMLNames,
+        xooMLIdx,
 	search,
         sychronized;
 
@@ -83,76 +84,88 @@ define([
 	  return callback(error);
 	}
 
-	realAssociations = associations.map(
+	itemAssociations = associations.map(
 	  function(assoc) {
-	    return { name: assoc.getDisplayText(),
-		     groupingItem: assoc.getIsGroupingItem() };
+	    return { name: assoc.commonData.localItem,
+		     groupingItem: assoc.commonData.isGrouping };
 	  }
 	);
 
-	// Associations with both names and guids
-	// filters out any phatoms
-	memoryAssociations = self._itemMirror.listAssociations()
-	  .map( function(guid) {
-	    return { guid: guid,
-		     name: self._itemMirror.getAssociationLocalItemName(guid) };
-	  })
-	  .filter( function(assoc) {
-	    return assoc.name !== null;
-	  });
+        self._xooMLDriver.getXooMLFragment(
+          function(error, xooMLContent) {
+            if (error) callback(error);
 
-	// No guarantee that the storage API sends results sorted
-	realAssociations.sort(self._nameCompare);
-	memoryAssociations.sort(self._nameCompare);
+           self._fragmentEditor = new FragmentEditor({text: xooMLContent});
 
-	// Gets the names a separate array, but in needed sorted order
-	realNames = realAssociations.map( function (assoc) {return assoc.name;} );
-	memoryNames = memoryAssociations.map( function (assoc) {return assoc.name;} );
-
-        // Keeps track of the index in the memoryassociations so that
-        // we don't waste time searching from the beginning
-	memoryIdx = 0;
-	// Keeps track of whether there are any changes that need to be made
-	sychronized = true;
-
-	realNames.forEach( function(name, realIdx) {
-	  search = memoryNames.indexOf(name, memoryIdx);
-	  // Create association
-	  if (search === -1) {
-	    sychronized = false;
-	    // Case 6/7
-	    self._fragmentEditor.createAssociation({
-	      displayText: name,
-	      itemName: name,
-	      isGroupingItem: realAssociations[realIdx].isGroupingItem
-	    });
-	  } else {
-	    // Deletes any extraneous associations
-	    memoryAssociations
-	      .slice(memoryIdx, search)
-	      .forEach( function(assoc) {
-		sychronized = false;
-		self._fragmentEditor.deleteAssociation(assoc.guid);
+	    // Associations with both names and guids
+	    // filters out any phatoms
+	    xooMLAssociations = Object.keys(self._fragmentEditor.associations)
+	      .map( function(guid) {
+	        return { guid: guid,
+		         name: self._itemMirror.getAssociationLocalItem(guid),
+                         groupingItem: self._itemMirror.isAssociationAssociatedItemGrouping(guid) };
+	      })
+	      .filter( function(assoc) {
+	        return assoc.name !== null;
 	      });
 
-	    memoryIdx = search;
-	  }
-	});
+	    // No guarantee that the storage API sends results sorted
+	    itemAssociations.sort(self._nameCompare);
+	    xooMLAssociations.sort(self._nameCompare);
 
-	// Any remaining associations need to be deleted because they don't exist
-	memoryAssociations
-	  .slice(memoryIdx, memoryNames.length)
-	  .forEach( function(assoc) {
-	    sychronized = false;
-	    self._fragmentEditor.deleteAssociation(assoc.guid);
-	  });
+	    // Gets the names a separate array, but in needed sorted order
+	    itemNames = itemAssociations.map( function (assoc) {return assoc.name;} );
+	    xooMLNames = xooMLAssociations.map( function (assoc) {return assoc.name;} );
 
-	// Only save fragment if needed
-	if (!sychronized) {
-	  self._itemMirror._saveFragment(callback);
-	}
+            // Keeps track of the index in the xooMLassociations so that
+            // we don't waste time searching from the beginning
+	    xooMLIdx = 0;
+	    // Keeps track of whether there are any changes that need to be made
+	    sychronized = true;
 
-        return callback;
+	    itemNames.forEach( function(name, itemIdx) {
+	      search = xooMLNames.indexOf(name, xooMLIdx);
+	      // Create association
+	      if (search === -1) {
+	        sychronized = false;
+	        // Case 6/7 only, other cases won't be handled
+                var association = new AssociationEditor({
+                  commonData: {
+	            displayText: name,
+	            localItem: name,
+	            isGroupingItem: itemAssociations[itemIdx].groupingItem
+                  }
+                });
+                self._fragmentEditor.associations[association.commonData.ID] = association;
+	      } else {
+	        // Deletes any extraneous associations
+	        xooMLAssociations
+	          .slice(xooMLIdx, search)
+	          .forEach( function(assoc) {
+		    sychronized = false;
+                    delete self._fragmentEditor.associations[assoc.guid];
+	          });
+	        xooMLIdx = search;
+	      }
+	    });
+
+	    // Any remaining associations need to be deleted because they don't exist
+	    xooMLAssociations
+	      .slice(xooMLIdx, xooMLNames.length)
+	      .forEach( function(assoc) {
+	        sychronized = false;
+                delete self._fragmentEditor.associations[assoc.guid];
+	      });
+
+	    // Only save fragment if needed
+	    if (!sychronized) {
+              self._fragmentEditor.updateID(); // generate a new guid for GUIDGeneratedOnLastWrite;
+              // Writes out the fragment
+              self._xooMLDriver.setXooMLFragment(self._fragmentEditor.toString());
+              callback(XooMLExceptions.itemMirrorNotCurrent);
+	    } else
+              callback(false);
+          });
       });
   };
 
