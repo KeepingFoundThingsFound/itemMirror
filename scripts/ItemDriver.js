168c62b45c03cc6a7968c2f89b5d8f9321925713
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-01-10 00:58:18 -0800

    Implemented createNonGroupingItem

--- scripts/ItemDriver.js
+++ scripts/ItemDriver.js
@@ -103,15 +103,58 @@ var AssociationEditor = require('./AssociationEditor');
    *
    * @protected
    */
-  ItemDriver.prototype.createNonGroupingItem = function (path, file, callback) {
+  ItemDriver.prototype.createNonGroupingItem = function (fileName, file, callback) {
     var self = this;
 
-    self._dropboxClient.writeFile(path, file, function (error, stat) {
-      if (error) {
-        return self._showDropboxError(error, callback);
-      }
-      return callback(false, stat);
-    });
+    function insertFile(fileData, callback) {
+      var boundary = '-------314159265358979323846';
+      var delimiter = "\r\n--" + boundary + "\r\n";
+      var close_delim = "\r\n--" + boundary + "--";
+
+      var reader = new FileReader();
+      reader.readAsBinaryString(fileData);
+      reader.onload = function() {
+        var contentType = fileData.type || 'application/octet-stream';
+        var metadata = {
+          'title': XooMLConfig.xooMLFragmentFileName,
+          'mimeType': contentType,
+          'parents': [{
+            "kind": "drive#parentReference",
+            "id": self._parentURI
+          }]
+        };
+
+        var base64Data = btoa(reader.result);
+        var multipartRequestBody =
+            delimiter +
+            'Content-Type: application/json\r\n\r\n' +
+            JSON.stringify(metadata) +
+            delimiter +
+            'Content-Type: ' + contentType + '\r\n' +
+            'Content-Transfer-Encoding: base64\r\n' +
+            '\r\n' +
+            base64Data +
+            close_delim;
+
+        var request = this.gapi.client.request({
+            'path': '/upload/drive/v2/files',
+            'method': 'POST',
+            'params': {'uploadType': 'multipart'},
+            'headers': {
+              'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+            },
+            'body': multipartRequestBody});
+        request.execute(function(response) {
+          callback(false, response);
+        }, function(response) {
+          callback('Could not write out File', response);
+        });
+      };
+    }
+
+    var blob = new Blob([file], {type: 'text/plain', fileName: fileName});
+
+    return insertFile(blob, callback);
   };
 
   /**
