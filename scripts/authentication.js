f5fe6cc1afb9e5dca6767adbd76ca650569e8123
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-04-21 22:31:40 -0700

    Added high level authentication functionality
    Authentication now will go through a higher level interface instead of relying
    entirely on the API of whatever globals we were previously importing. This is
    providing the main utility for IM as a static method. The idea is that users can
    call the authenticate method on a given service, and then have a standard
    interface for resuming upon completion.
    
    The main functionality that this is missing, but is needed to work properly is
    the generation of a proper URI for starting the auth flow. Since that can differ
    between services and has it's own complexity, I've decided to leave it out and
    instead use a simple URI parameter, so the high level IM method can generate the
    URI and pass it on to this function.

---
+++ scripts/authentication.js
@@ -0,0 +1,59 @@
+var localStoragePrelude = 'item-mirror-service:'
+
+function getKey (service) {
+  return localStoragePrelude + service
+}
+
+function isAuthenticated (service) {
+  // If there's a value, assume we're authenticated
+  return !!(getKey(service))
+}
+
+/**
+ * @private
+ * @static
+ * @method authenticate
+ * @param {string} service The service that is getting authenticated
+ * @param {string} uri The URI that is used to start the initial auth flow. This
+ * should come from an authorization driver that can intelligently create a URI.
+ * @param {Function} callback Callback for completion of the authentication
+ * process
+ * @param {boolean} force (Optional) whether we should ignore a token that's
+ * already in localStorage. Might be needed if we need to refresh credentials
+ * @returns {Function} Returns a handler that should be attached to a UI element
+ * and allows the user to initiate the authentication process. Has an additional
+ * side effect of creating an event listener and binding it to the window so
+ * that the callback passed in is executed after the auth flow.
+ */
+function authenticate (service, uri, callback, force) {
+  // First check if we're already authenticated
+  if (!force && isAuthenticated(service)) {
+    return callback(false)
+  }
+
+  // Make an event handler that we pass back
+  var handler = function () {
+    window.open(uri)
+  }
+
+  // Bind an event to the DOM for handling completion
+  window.addEventListener('storage', function (evt) {
+    if (evt.key === getKey(service)) {
+      // This means authentication somehow went wrong
+      if (evt.newValue === 'false') { // It's a string because localStorage ONLY allows strings
+        return callback(new Error('Authentication Failed'))
+      }
+
+      callback(false)
+    }
+  })
+
+  return handler
+}
+
+module.exports = {
+  // Returns whether we're authenticated for the service
+  isAuthenticated: isAuthenticated,
+  // Performs the authentication process
+  authenticate: authenticate
+}
