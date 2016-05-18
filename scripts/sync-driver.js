b9430eb0f4f3dfd2f2ae51c226bcf595f143b307
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-05-17 22:02:25 -0700

    Reimplements sync method

--- scripts/sync-driver.js
+++ scripts/sync-driver.js
@@ -19,130 +19,44 @@
 
 var FragmentEditor = require('./fragment-editor')
 
+/**
+ * @class SyncDriver
+ * @property {XooMLDriver} xooMLDriver
+ * @property {string} guid The guid generated on last write by the itemMirror
+ * instance.
+ * @param {ItemMirror} itemMirror The calling ItemMirror instance should pass
+ * itself. This is really just so that we can pass the XooMLDriver associated
+ * with it, as well as the GUID
+ */
 function SyncDriver (itemMirror) {
-  var self = this
-  self._itemMirror = itemMirror
-  self._itemDriver = itemMirror._itemDriver
-  self._xooMLDriver = itemMirror._xooMLDriver
+  // Assigns the xooml driver as a property for easy access
+  this.xooMLDriver = itemMirror._xooMLDriver
+  this.itemMirror = itemMirror
+  this.guid = itemMirror._fragment.commonData.GUIDGeneratedOnLastWrite
 }
 
-  /**
-   * Helper method that allows for sorting of objects by the localItem
-   *
-   * @method _nameCompare
-   * @private
-   * @protected
-   */
-function _localItemCompare (a, b) {
-  if (a.commonData.localItem > b.commonData.localItem) return 1
-  else if (a.commonData.localItem < b.commonData.localItem) return -1
-  else return 0
-}
-
-  /**
-   * Synchonizes the itemMirror object.
-   *
-   * @method sync
-   *
-   * @param {Function} callback Function to execute once finished.
-   *  @param {Object}   callback.error Null if no error has occurred
-   *                    in executing this function, else an contains
-   *                    an object with the error that occurred.
-   *
-   * @protected
-   */
-SyncDriver.prototype.sync = function (callback) {
+/**
+ * Synchonizes the itemMirror object, or fails trying
+ *
+ * @method sync
+ * @returns {Promise} Returns a promise that resolves when the itemMirror
+ * object is synchonized. If a sync fails (because the
+ * GUIDGeneratedOnLastWrite) differs between itemMirrors, then the promise
+ * will be an _error_
+ */
+SyncDriver.prototype.sync = function () {
   var self = this
-  var itemAssociations
-
-  self._itemDriver.listItems(self._itemMirror._groupingItemURI,
-                               processItems)
-
-  function processItems (error, associations) {
-    if (error) return callback(error)
-
-    itemAssociations = associations
-    self._xooMLDriver.getXooMLFragment(processXooML)
-  }
-
-  function processXooML (error, xooMLContent) {
-      // A 404 error is dropbox telling us that the file doesn't
-      // exist. In that case we just write the file
-    if (error === 404) {
-      var fragmentString = self._itemMirror._fragment.toString()
-      return self._xooMLDriver.setXooMLFragment(fragmentString, function (error) {
-        if (error) callback(error)
-        else callback(false)
-      })
-    } else if (error) {
-      return callback(error)
+  // Use the xoomldriver to get the 'latest' version of the XML as it's
+  // persisted
+  return this.xooMLDriver.getXooMLFragment(function (xml) {
+    // With the parsed the XML, and get the GUIDGeneratedOnLastWrite
+    var fragment = new FragmentEditor({ 'text': xml })
+    if (fragment.commonData.GUIDGeneratedOnLastWrite === self.guid) {
+      return // Yay, everything is in sync!
+    } else {
+      throw new Error('Sync Error')
     }
-
-      // Keeps track of the index in the xooMLassociations so that
-      // we don't waste time searching from the beginning
-    var xooMLIdx = 0
-      // Keeps track of whether there are any changes that need to be made
-    var synchronized = true
-    var xooMLAssociations
-
-    self._fragmentEditor = new FragmentEditor({text: xooMLContent})
-
-    xooMLAssociations = Object.keys(self._fragmentEditor.associations)
-      // Turns the associative array into a regular array for iteration
-        .map(function (guid) {
-          return self._fragmentEditor.associations[guid]
-        })
-      // filters out any phantoms
-  .filter(function (assoc) {
-    return assoc.commonData.localItem !== null
   })
-
-      // No guarantee that the storage API sends results sorted
-    itemAssociations.sort(_localItemCompare)
-    xooMLAssociations.sort(_localItemCompare)
-
-      // Gets the localItems in a separate array, but in needed sorted order
-    var itemLocals = itemAssociations.map(function (assoc) { return assoc.commonData.localItem })
-    var xooMLLocals = xooMLAssociations.map(function (assoc) { return assoc.commonData.localItem })
-
-    itemLocals.forEach(function (localItem, itemIdx) {
-      var search = xooMLLocals.lastIndexOf(localItem, xooMLIdx)
-  // Create association
-      if (search === -1) {
-        synchronized = false
-    // Case 6/7 only, other cases won't be handled
-        var association = itemAssociations[itemIdx]
-        self._fragmentEditor.associations[association.commonData.ID] = association
-      } else {
-    // Deletes any extraneous associations
-        xooMLAssociations
-      .slice(xooMLIdx, search)
-      .forEach(function (assoc) {
-        synchronized = false
-        delete self._fragmentEditor.associations[assoc.guid]
-      })
-        xooMLIdx = search + 1
-      }
-    })
-      // Any remaining associations need to be deleted because they don't exist
-    xooMLAssociations
-  .slice(xooMLIdx, xooMLLocals.length)
-  .forEach(function (assoc) {
-    synchronized = false
-    delete self._fragmentEditor.associations[assoc.commonData.ID]
-  })
-
-      // Only save fragment if needed
-    if (!synchronized) {
-      self._fragmentEditor.updateID() // generate a new guid for GUIDGeneratedOnLastWrite;
-        // Writes out the fragment
-      self._xooMLDriver.setXooMLFragment(self._fragmentEditor.toString(), function (error) {
-        if (error) return callback(error)
-
-        return callback(false)
-      })
-    } else return callback(false)
-  }
 }
 
 module.exports = SyncDriver
